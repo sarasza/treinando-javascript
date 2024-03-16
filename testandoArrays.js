cc8cacda4c6ecfcba02b98beab646f7878f33bb9
sarasza <sarasouza1516@gmail.com>
2024-03-16 17:10:07 -0300

    novos treinos

---
+++ testandoArrays.js
@@ -0,0 +1,10 @@
+const listaDeDestinos = new Array (`Fortaleza`, `Curitiba`, `São Paulo`)
+console.log(listaDeDestinos)
+
+listaDeDestinos.splice(1, 1)
+console.log(listaDeDestinos)
+
+listaDeDestinos.push(`Curitiba`)
+console.log(listaDeDestinos)
+
+console.log(listaDeDestinos[2])
