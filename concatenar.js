33ac6abd879312ec30e112ad4b88a027fb87b93f
sarasza <sarasouza1516@gmail.com>
2024-03-16 16:53:04 -0300

    novo programa

---
+++ concatenar.js
@@ -0,0 +1,9 @@
+const almoco = "lasanha"
+const janta = "pizza"
+console.log(`Hoje, eu almocei ` + almoco + `. Mais tarde irei jantar ` + janta + `!`)
+
+/* testando de outra forma */
+
+const primeiroNome = "Sara"
+const sobrenome = "Souza"
+console.log(`${primeiroNome} ${sobrenome}`)
